6edba486f4998d891b09246deaf8ddc034b2d6d1
AlicanAkyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2019-01-21 13:39:13 -0800

    ducks.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
 import './Header.scss';
 
 const Header = (props) => (
   <header className="header">
-    <img src={logo} alt="Sauce Labs" className="logo" />
   </header>
 );
 
